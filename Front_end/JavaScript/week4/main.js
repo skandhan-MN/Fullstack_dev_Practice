2570ec5e8da7dcfe3a74c351d5f546a465d23d8e
Skandhan89 <54491902+Skandhan89@users.noreply.github.com>
2022-11-18 07:30:29 +0530

    builtin Functions

---
+++ Front_end/JavaScript/week4/main.js
@@ -0,0 +1,9 @@
+//Function and built functions//
+
+let myString = "skandhan rohan pradhyun sahas";
+step1 = myString.toUpperCase();
+console.log(step1);
+step2 = step1.split(" ");
+console.log(step2);
+step3 = step2.indexOf("ROHAN");
+console.log(step3);
